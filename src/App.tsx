937ad61fb1b8f796b29d733f24d67ecb1c8db8e4
Pulinda Mathagadeera <122967894+Puliya99@users.noreply.github.com>
2026-03-08 00:31:47 +0530

    feat: add @vercel/speed-insights for performance monitoring in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from './context/AuthContext'
 import { ThemeProvider } from './context/ThemeContext'
 import AppRoutes from './routes/AppRoutes'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
   return (
@@ -12,11 +13,13 @@ function App() {
         <AuthProvider>
           <div className="App">
             <AppRoutes />
+
             <Toaster
               position="top-right"
               toastOptions={{
                 duration: 4000,
-                 className: 'bg-white text-gray-900 dark:bg-gray-800 dark:text-white',
+                className:
+                  'bg-white text-gray-900 dark:bg-gray-800 dark:text-white',
                 success: {
                   duration: 3000,
                   iconTheme: {
@@ -33,6 +36,8 @@ function App() {
                 },
               }}
             />
+
+            <SpeedInsights />
           </div>
         </AuthProvider>
       </ThemeProvider>
